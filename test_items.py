1dea074cb637ab841435f3c3c0464a367049c641
GuzaliaV <168180094+GuzaliaV@users.noreply.github.com>
2024-09-23 19:25:00 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,11 +1,15 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-browser = webdriver.Chrome()
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-browser.get(link)
 
-button = (browser.find_element(By.XPATH, "//a[@href='/ru/basket/]'"))
-button.click()
+def test_add_to_basket_button(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
+    browser.get(link)
+
+    add_to_basket_button = WebDriverWait(browser, 30).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "btn-add-to-basket"))
+    )
+
+    assert add_to_basket_button.text == 'Ajouter au panier'
